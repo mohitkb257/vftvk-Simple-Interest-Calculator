1fff3ad40ce62d81235903f895ccd8ec51b4a285
mohitkb257 <98774637+mohitkb257@users.noreply.github.com>
2022-02-02 11:38:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ function compute()
     p = document.getElementById("principal").value;
 	
 }
+
+<script>
+var slider = document.getElementById("rate");
+var output = document.getElementById("rateDisplay");
+output.innerHTML = concat(slider.value,"%");
+
+slider.oninput = function() {
+	output.innerHTML = concat(this.value,"%");
+}
+</script>
