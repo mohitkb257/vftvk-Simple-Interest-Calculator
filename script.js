47a61da8f563258c473d442d07e582a09f3cb519
mohitkb257 <98774637+mohitkb257@users.noreply.github.com>
2022-02-02 13:54:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-function compute() {
-  p = document.getElementById("principal").value;
-
-}
-
 var slider = document.getElementById("rate");
 var output = document.getElementById("rateDisplay");
 output.innerHTML = slider.value;
 
 slider.oninput = function() {
   output.innerHTML = this.value;
+}
+
+function compute() {
+  p = document.getElementById("principal").value;
+	y = document.getElementById("years").value;
+  r = document.getElementById("rate").value;
+ 
+  let total = p * r * y / 100;
+  
+  let matYear = 2022 + y;
+  
+  document.getElementById("result").innerHTML = "If you deposit " + p + "\nat an interest rate of " + r + "%,\nyou will receive an amount of " + total + "\nin the year " + matYear;
+  
 }
